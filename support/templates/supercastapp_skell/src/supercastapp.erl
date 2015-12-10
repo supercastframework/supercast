6df6600d821b89586ea259dbd10fd0c74ac73862
Sébastien Serre <sserre.bx@gmail.com>
2015-12-10 02:31:12 +0100

    final API reflexion

--- support/templates/supercastapp_skell/src/supercastapp.erl
+++ support/templates/supercastapp_skell/src/supercastapp.erl
@@ -1,27 +1,52 @@
-%% @doc {{appid}}.
+%% @doc The wonderfull {{appid}} application.
 
 -module({{appid}}).
 -author("{{author}}").
--export([start/0, stop/0]).
+-export([init/0]).
 
-ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
+%% supercast channel behaviour
+-export([synchronize/3,disconnect/3]).
 
+%% user test
+-export([broadcast/2, unicast/2]).
 
 %% @spec start() -> ok
 %% @doc Start the {{appid}} server.
 start() ->
-    {{appid}}_deps:ensure(),
-    ensure_started(crypto),
-    application:start({{appid}}).
+    ok = application:start(xmerl),
+    ok = application:start(supercast),
+    ok = supercast:listen(),
+    ChanName = "{{appid}}",
+    Perm = undefined,
+    Opts = [],
+    ok = supercast:create(ChanName, ?MODULE, Opts, Perm),
+    %% ok = supercast:set_opts(ChanName, Opts),
+    %% ok = supercast:delete(ChanName)
+
+%% @spec synchronize(ChanName::string(), Opts::any(), ClientState::any()) ->
+%%      {ok, Pdus::[term()]} | {error, Reason::term()}
+synchronize("{{appid}}", _Opts, _CState) ->
+    {ok, ["hello from jojo", "you should be synchro now"]};
+synchronize(_, _, _) ->
+    {error, "unknown channel"}.
+
+%% @spec leave(Channel, Opts, CState) ->
+%%      ok | {error, Reason::string()}
+disconnect("{{appid}}", _Opts, _CState) -> ok;
+disconnect(_Channel, _Opts, _CState) -> {error, "unknown channel"}.
 
 
-%% @spec stop() -> ok
-%% @doc Stop the {{appid}} server.
-stop() ->
-    application:stop({{appid}}).
+%% @spec send_broadcast(Messages::[term()], Perm::any()) -> ok
+broadcast(Messages, undefined) ->
+    Channel = "{{appid}}",
+    supercast:broadcast(Channel, Messages);
+broadcast(Messages, CustomPerm) ->
+    Channel = "{{appid}}",
+    supercast:broadcast(Channel, Messages, CustomPerm).
+
+%% @spec send_unicast(Messages::[term()], CState) -> ok
+unicast(Messages, CState) ->
+    Channel = "{{appid}}",
+    supercast:unicast(Channel, Messages, CState).
+
+
--- support/templates/supercastapp_skell/start-dev.sh
+++ support/templates/supercastapp_skell/start-dev.sh
@@ -3,5 +3,4 @@ exec erl \
     -pa ebin deps/*/ebin \
     -boot start_sasl \
     -sname {{appid}}_dev \
-    -s {{appid}} \
-    -s reloader
+    -eval "{{appid}}:start()."
